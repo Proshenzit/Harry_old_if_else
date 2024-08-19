ec30fc1eacb55fdf5dff9e5301c7fc3930aa18b0
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-08-18 23:12:10 -0700

    Add files via upload

---
+++ Harry_old_if_else.py
@@ -0,0 +1,28 @@
+''' var1 =6
+var2= 56
+var3 = int (input("Enter your number: "))
+if var3 > var2:
+    print("Greater ")
+if var3==var2:
+        print("Equal")
+else:
+    print("Lesser") '''
+
+''' list =[5,6,6,7,87,]
+
+print(5 in list )
+if  5 in list:
+    print("yes  it is the list ")
+'''
+
+
+
+print( "what is your age? ")
+age = int (input('Enter  your age: '))
+if age <18:
+    print("You cannot drive")
+elif age== 18:
+    print("we will think  about ")
+
+else:
+    print("you can drive ")
